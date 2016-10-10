44a0d034df4ad49619a18d4f9324680a0b8f8c90
Phill Conrad <pconrad@cs.ucsb.edu>
2016-10-10 13:07:39 -0700

    perm to student hash table ok

--- HashTableDemo.java
+++ HashTableDemo.java
@@ -2,7 +2,11 @@ import java.util.HashMap;
 
 public class HashTableDemo {
 
-
+    public static void addToHashMap
+	(HashMap<Integer,Student> permToStudent,
+	 Student s) {
+	permToStudent.put(s.getPerm(),s);
+    }
     
     public static void main(String [] args) {
 
@@ -15,9 +19,12 @@ public class HashTableDemo {
 	Student cg = new Student(1234567,"Chris Gaucho");
 	Student td = new Student(3333333,"Trey De La Playa");
 	Student pp = new Student(4444444,"Pat Pardall");
-	
 
-	
+	addToHashMap(permToStudent,cg);
+	addToHashMap(permToStudent,td);
+	addToHashMap(permToStudent,pp);
+
+	System.out.println("permToStudent: " + permToStudent);
     }
     
 }
