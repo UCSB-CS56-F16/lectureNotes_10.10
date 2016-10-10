0a8d62951a11947e158e697918955ac4b1118673
Phill Conrad <pconrad@cs.ucsb.edu>
2016-10-10 13:14:57 -0700

    Added demo of what overriding hashCode in Student really does

--- HashTableDemo.java
+++ HashTableDemo.java
@@ -17,14 +17,26 @@ public class HashTableDemo {
 	    new HashMap<Integer,Student> ();
 
 	Student cg = new Student(1234567,"Chris Gaucho");
+	studentToMajor.put(cg,"CMPSC");
 	Student td = new Student(3333333,"Trey De La Playa");
+	studentToMajor.put(td,"CMPEN");
 	Student pp = new Student(4444444,"Pat Pardall");
+	studentToMajor.put(pp,"MATH");
+
+	Student pp2 = new Student(4444444,"Pat Pardall");
 
 	addToHashMap(permToStudent,cg);
 	addToHashMap(permToStudent,td);
 	addToHashMap(permToStudent,pp);
 
 	System.out.println("permToStudent: " + permToStudent);
+	System.out.println("studentToMajor " + studentToMajor);
+
+
+	String whatMajor = studentToMajor.get(pp2);
+
+	System.out.println("pp2's major is " + whatMajor);
+
     }
     
 }
