7e53b0b80c0c117d9502615288c404fd2c80ca35
Phill Conrad <pconrad@cs.ucsb.edu>
2016-10-10 12:57:09 -0700

    XorDemo plus WIP on HashTableDemo

---
+++ HashTableDemo.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+public class HashTableDemo {
+
+
+    
+    public static void main(String [] args) {
+
+	HashMap<Student,String> studentToMajor =
+	    new HashMap<Student,String> ();
+
+	HashMap<Integer,Student> permToStudent=
+	    new HashMap<Integer,Student> ();
+
+	Student cg = new Student(1234567,"Chris Gaucho");
+	Student td = new Student(3333333,"Trey De La Playa");
+	Student pp = new Student(4444444,"Pat Pardall");
+	
+
+	
+    }
+    
+}
---
+++ XorDemo.java
@@ -0,0 +1,15 @@
+public class XorDemo {
+
+public static void main(String [] args) {
+
+
+  System.out.println("3 ^ 6) = " + (3^6));
+
+  System.out.println("1 ^ 6) = " + (1^6));
+
+  System.out.println("3 ^ 7) = " + (3^7));
+  System.out.println("3 ^ 5) = " + (3^5));
+
+}
+
+}
